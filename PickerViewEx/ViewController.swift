098b9755c19dc2c7a38ecd04bd1784ee765142a0
jinunzzan <lovebox6607@gmail.com>
2022-10-25 02:43:34 +0900

    PickerView

--- PickerViewEx/ViewController.swift
+++ PickerViewEx/ViewController.swift
@@ -7,11 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource
 
+{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0 {
+            return 3
+        } else {
+            return 5
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "com:\(component),row:\(row)"
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        label.text = "com:\(component),row:\(row)"
+    }
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 50
+    }
+    
+
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var picker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        picker.dataSource = self
+        picker.delegate = self
     }
 
 
